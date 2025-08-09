5f8a8c68e7bbcce0656243a7c6237afdb1e6840c
cihuyyama <miqbalalhabib@gmail.com>
2025-08-09 14:44:20 +0700

    cors

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ server.register(cors, {
     origin: `${process.env.NODE_ENV === 'production' ? process.env.FRONTEND_URL : 'http://localhost:3000'}`,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: '*',
+    allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
 })
 
 server.ready(err => {
